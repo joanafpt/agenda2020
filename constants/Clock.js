bab6730443a8bfa35ac604f4b023e0f7bef8f37b
joanafpt <joanajteixeira@outlook.com>
2020-04-27 15:07:38 +0100

    Clock

---
+++ constants/Clock.js
@@ -0,0 +1,25 @@
+export default {
+    clockContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        backgroundColor: 'white',
+        marginBottom: 5
+    },
+    twoPoints: {
+        marginLeft: 2,
+        marginRight: 2
+    },
+    text: {
+        fontSize: 30,
+        alignContent: 'center',
+    },
+    clockInput: {
+        borderWidth: 1,
+        borderColor: '#B7B7B7',
+        width: 76,//era 70 
+        height: 40,
+        marginTop: 2,
+        marginBottom: 1,
+        textAlign: 'center',
+    },
+}
