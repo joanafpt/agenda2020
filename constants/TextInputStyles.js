214f23cc6f721099ae55948111931dc0181d3ed3
joanafpt <joanajteixeira@outlook.com>
2020-04-27 15:09:55 +0100

    TextInputStyles

---
+++ constants/TextInputStyles.js
@@ -0,0 +1,10 @@
+export default {
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#B7B7B7',
+        width: 200,
+        height: 40,
+        marginTop: 2,
+        marginBottom: 1
+    },
+}
