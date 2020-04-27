e52f331fd48e48908e5eff63cf57acddc1b36226
joanafpt <joanajteixeira@outlook.com>
2020-04-27 15:09:38 +0100

    OutputResponses

---
+++ constants/OutputsResponses.js
@@ -0,0 +1,17 @@
+export default {
+    display: {
+        marginTop: 4,
+        width: 200,
+        height: 80,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    displayText: {
+        fontSize: 18
+    },
+    noResults: {
+        textAlign: "center",
+
+    },
+}
