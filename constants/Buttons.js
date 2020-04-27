13f7cdcc34257b35a0150d7efae93add976f6b3e
joanafpt <joanajteixeira@outlook.com>
2020-04-27 15:07:29 +0100

    Buttons

---
+++ constants/Buttons.js
@@ -0,0 +1,41 @@
+export default {
+    buttonsContainer: {
+        width: 300,
+        height: 60,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white'
+    },
+    firstButton: {
+        marginRight: 5,
+        width: 100,
+        backgroundColor: '#0FCFF3',
+        height: 50,
+        borderRadius: 20,
+        alignItems: "center",
+        padding: 15,
+      
+    },
+    secondButton: {
+        marginLeft: 5,
+        width: 100,
+        backgroundColor: '#0FCFF3',
+        height: 50,
+        borderRadius: 20,
+        alignItems: "center",
+        padding: 15
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        textAlign: 'center'
+
+    },
+   
+
+   }
+   
+   
+   
