57c110805fcbeff8b68f44ea18a0c36fad8cc690
joanafpt <joanajteixeira@outlook.com>
2020-04-27 15:09:22 +0100

    HeaderStyles

---
+++ constants/HeaderStyles.js
@@ -0,0 +1,13 @@
+export default {
+    header: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#0FCFF3',
+        height: '15%',
+    },
+    headerText: {
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 20
+    },
+}
